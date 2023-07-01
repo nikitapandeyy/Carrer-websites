f50908b74a1220822829a1bb4e13caecaa95e1ea
nikitapandeyy <113332472+nikitapandeyy@users.noreply.github.com>
2023-07-01 03:06:49 +0000

    i am so happy

--- database.py
+++ database.py
@@ -17,30 +17,27 @@ def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
         jobs = []
+        columns = result.keys()  # Get the column names
+        
         for row in result:
-            jobs.append(dict(row._asdict()))  # Convert row to dictionary
+            job = dict(zip(columns, row))  # Create a dictionary using column names and row values
+            jobs.append(job)
+        
         return jobs
 
 
 
 def load_job_from_db(id):
     with engine.connect() as conn:
-        query = text("SELECT * FROM jobs WHERE id = :id")
-        result = conn.execute(query, {"id": id})
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {'val': id}
+        )
         rows = result.fetchall()
         if len(rows) == 0:
             return None
         else:
-            row = rows[0]
-            job = {}
-            for column, value in row.items():
-                # Update column name here
-                if column == "job_items":
-                    job["items"] = value
-                else:
-                    job[column] = value
-            return job
-
+            return rows[0]
 
 
 
@@ -66,6 +63,6 @@ def add_application_to_db(job_id, data):
 
 
 
-"""engine = create_engine("mysql+pymysql://ne252ylmd96n7j25o1ww:pscale_pw_7VCE2mgERR6cX7QiLJh7KiTrPRAwgYeacHuylrWK9zC@aws.connect.psdb.cloud/careerwebsite?charset=utf8mb4")"""
+
    
    
